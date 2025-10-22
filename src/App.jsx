ddf3b8d9d56ac73c723b4de162176ed362818f6a
Jotta2k4 <joao80615@gmail.com>
2025-10-22 20:53:51 -0300

    props e useState

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import ComponenteTreino from "./components/ComponenteTreino";
+
+function App() {
+  return (
+
+    <div className="App">
+
+        <ComponenteTreino />
+
+    </div>
+  )
+}
+
+export default App;
---
+++ src/components/ComponenteTreino.jsx
@@ -0,0 +1,66 @@
+import React from "react";
+
+function ComponenteTreino(props) {
+
+    //Verficador de imagens
+
+    // const onClickBotao = () => {
+    //     const input = document.getElementById('input');
+    //     const valorDoInput = input.value ? input.value.trim() : '';
+    //     const comecaComHttp = valorDoInput.startsWith('http://') || input.value.startsWith('https://');
+    //     const terminaComJpgOuPng = valorDoInput.endsWith('.jpg') || input.value.endsWith('.png');
+    //     const campoDaImagem = document.getElementById('campoDaImagem');
+
+    //     if (comecaComHttp && terminaComJpgOuPng) {
+    //         const imagem = document.createElement('img');
+    //         imagem.src = valorDoInput; 
+    //         campoDaImagem.appendChild(imagem);
+    //     } else {
+    //         alert("Insira um link de imagem válido!");
+    //     }
+    // }
+
+    // return (
+    //     <div>
+    //         <section>
+    //             <h1>{props.titulo}</h1>
+    //             <h3>{props.subtitulo}</h3>
+    //             <input id='input' type="text" />
+    //             <button onClick={onClickBotao}>Enviar</button>
+    //         </section>
+    //         <span id='campoDaImagem'>
+
+    //         </span>
+
+    //     </div>
+    // )
+
+    //----------------------------------------------------------------------------------------
+
+    //Contador
+
+    // const [valor, setValor] = React.useState(0);
+
+    // const addNum = () => {
+    //     setValor(valor + 1);
+    // }
+
+    // const removeNum = () => {
+    //     setValor(valor - 1);
+    // }
+
+    // return (
+    //     <div>
+    //     <h1>Contador</h1>
+    //     <p>{valor}</p>
+    //     <button onClick={addNum}>Add</button>
+    //     <button onClick={removeNum}>Remove</button></div>
+
+    // )
+
+    //----------------------------------------------------------------------------------------
+
+    //Lista de compras
+}
+
+export default ComponenteTreino;
